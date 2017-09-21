bcfec1069bf6850db81691e19b5b531d4f245197
Vincent Hanquez <vincent@snarc.org>
2017-09-21 10:08:52 +0100

    merge statistics

---
+++ statistics/Statistics/Distribution/Normal.hs
@@ -0,0 +1,161 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveDataTypeable, DeriveGeneric #-}
+-- |
+-- Module    : Statistics.Distribution.Normal
+-- Copyright : (c) 2009 Bryan O'Sullivan
+-- License   : BSD3
+--
+-- Maintainer  : bos@serpentine.com
+-- Stability   : experimental
+-- Portability : portable
+--
+-- The normal distribution.  This is a continuous probability
+-- distribution that describes data that cluster around a mean.
+
+module Statistics.Distribution.Normal
+    (
+      NormalDistribution
+    -- * Constructors
+    , normalDistr
+    , normalDistrE
+    , standard
+    ) where
+
+import Control.Applicative
+import Data.Data             (Data, Typeable)
+import GHC.Generics          (Generic)
+import Numeric.MathFunctions.Constants (m_sqrt_2, m_sqrt_2_pi)
+import Numeric.SpecFunctions (erfc, invErfc)
+import qualified System.Random.MWC.Distributions as MWC
+import qualified Data.Vector.Generic as G
+
+import qualified Statistics.Distribution as D
+import qualified Statistics.Sample as S
+import Statistics.Internal
+
+
+-- | The normal distribution.
+data NormalDistribution = ND {
+      mean       :: {-# UNPACK #-} !Double
+    , stdDev     :: {-# UNPACK #-} !Double
+    , ndPdfDenom :: {-# UNPACK #-} !Double
+    , ndCdfDenom :: {-# UNPACK #-} !Double
+    } deriving (Eq, Typeable, Data, Generic)
+
+instance Show NormalDistribution where
+  showsPrec i (ND m s _ _) = defaultShow2 "normalDistr" m s i
+instance Read NormalDistribution where
+  readPrec = defaultReadPrecM2 "normalDistr" normalDistrE
+
+instance D.Distribution NormalDistribution where
+    cumulative      = cumulative
+    complCumulative = complCumulative
+
+instance D.ContDistr NormalDistribution where
+    logDensity    = logDensity
+    quantile      = quantile
+    complQuantile = complQuantile
+
+instance D.MaybeMean NormalDistribution where
+    maybeMean = Just . D.mean
+
+instance D.Mean NormalDistribution where
+    mean = mean
+
+instance D.MaybeVariance NormalDistribution where
+    maybeStdDev   = Just . D.stdDev
+    maybeVariance = Just . D.variance
+
+instance D.Variance NormalDistribution where
+    stdDev = stdDev
+
+instance D.Entropy NormalDistribution where
+  entropy d = 0.5 * log (2 * pi * exp 1 * D.variance d)
+
+instance D.MaybeEntropy NormalDistribution where
+  maybeEntropy = Just . D.entropy
+
+instance D.ContGen NormalDistribution where
+    genContVar d = MWC.normal (mean d) (stdDev d)
+
+-- | Standard normal distribution with mean equal to 0 and variance equal to 1
+standard :: NormalDistribution
+standard = ND { mean       = 0.0
+              , stdDev     = 1.0
+              , ndPdfDenom = log m_sqrt_2_pi
+              , ndCdfDenom = m_sqrt_2
+              }
+
+-- | Create normal distribution from parameters.
+--
+-- IMPORTANT: prior to 0.10 release second parameter was variance not
+-- standard deviation.
+normalDistr :: Double            -- ^ Mean of distribution
+            -> Double            -- ^ Standard deviation of distribution
+            -> NormalDistribution
+normalDistr m sd = maybe (error $ errMsg m sd) id $ normalDistrE m sd
+
+-- | Create normal distribution from parameters.
+--
+-- IMPORTANT: prior to 0.10 release second parameter was variance not
+-- standard deviation.
+normalDistrE :: Double            -- ^ Mean of distribution
+             -> Double            -- ^ Standard deviation of distribution
+             -> Maybe NormalDistribution
+normalDistrE m sd
+  | sd > 0    = Just ND { mean       = m
+                        , stdDev     = sd
+                        , ndPdfDenom = log $ m_sqrt_2_pi * sd
+                        , ndCdfDenom = m_sqrt_2 * sd
+                        }
+  | otherwise = Nothing
+
+errMsg :: Double -> Double -> String
+errMsg _ sd = "Statistics.Distribution.Normal.normalDistr: standard deviation must be positive. Got " ++ show sd
+
+-- | Variance is estimated using maximum likelihood method
+--   (biased estimation).
+--
+--   Returns @Nothing@ if sample contains less than one element or
+--   variance is zero (all elements are equal)
+instance D.FromSample NormalDistribution Double where
+  fromSample xs
+    | G.length xs <= 1 = Nothing
+    | v == 0           = Nothing
+    | otherwise        = Just $! normalDistr m (sqrt v)
+    where
+      (m,v) = S.meanVariance xs
+
+logDensity :: NormalDistribution -> Double -> Double
+logDensity d x = (-xm * xm / (2 * sd * sd)) - ndPdfDenom d
+    where xm = x - mean d
+          sd = stdDev d
+
+cumulative :: NormalDistribution -> Double -> Double
+cumulative d x = erfc ((mean d - x) / ndCdfDenom d) / 2
+
+complCumulative :: NormalDistribution -> Double -> Double
+complCumulative d x = erfc ((x - mean d) / ndCdfDenom d) / 2
+
+quantile :: NormalDistribution -> Double -> Double
+quantile d p
+  | p == 0         = -inf
+  | p == 1         = inf
+  | p == 0.5       = mean d
+  | p > 0 && p < 1 = x * ndCdfDenom d + mean d
+  | otherwise      =
+    error $ "Statistics.Distribution.Normal.quantile: p must be in [0,1] range. Got: "++show p
+  where x          = - invErfc (2 * p)
+        inf        = 1/0
+
+complQuantile :: NormalDistribution -> Double -> Double
+complQuantile d p
+  | p == 0         = inf
+  | p == 1         = -inf
+  | p == 0.5       = mean d
+  | p > 0 && p < 1 = x * ndCdfDenom d + mean d
+  | otherwise      =
+    error $ "Statistics.Distribution.Normal.complQuantile: p must be in [0,1] range. Got: "++show p
+  where x          = invErfc (2 * p)
+        inf        = 1/0
---
+++ statistics/Statistics/Resampling/Bootstrap.hs
@@ -0,0 +1,107 @@
+-- |
+-- Module    : Statistics.Resampling.Bootstrap
+-- Copyright : (c) 2009, 2011 Bryan O'Sullivan
+-- License   : BSD3
+--
+-- Maintainer  : bos@serpentine.com
+-- Stability   : experimental
+-- Portability : portable
+--
+-- The bootstrap method for statistical inference.
+
+module Statistics.Resampling.Bootstrap
+    ( bootstrapBCA
+    , basicBootstrap
+    -- * References
+    -- $references
+    ) where
+
+import           Data.Vector.Generic ((!))
+import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Generic as G
+
+import Statistics.Distribution (cumulative, quantile)
+import Statistics.Distribution.Normal
+import Statistics.Resampling (Bootstrap(..), jackknife)
+import Statistics.Sample (mean)
+import Statistics.Types (Sample, CL, Estimate, ConfInt, estimateFromInterval,
+                         estimateFromErr, CL, significanceLevel)
+import Statistics.Function (gsort)
+
+import qualified Statistics.Resampling as R
+
+
+data T = {-# UNPACK #-} !Double :< {-# UNPACK #-} !Double
+infixl 2 :<
+
+-- | Bias-corrected accelerated (BCA) bootstrap. This adjusts for both
+--   bias and skewness in the resampled distribution.
+--
+--   BCA algorithm is described in ch. 5 of Davison, Hinkley "Confidence
+--   intervals" in section 5.3 "Percentile method"
+bootstrapBCA
+  :: CL Double       -- ^ Confidence level
+  -> Sample          -- ^ Full data sample
+  -> [(R.Estimator, Bootstrap U.Vector Double)]
+  -- ^ Estimates obtained from resampled data and estimator used for
+  --   this.
+  -> [Estimate ConfInt Double]
+bootstrapBCA confidenceLevel sample resampledData
+  = map e resampledData
+  where
+    e (est, Bootstrap pt resample)
+      | U.length sample == 1 || isInfinite bias =
+          estimateFromErr      pt (0,0) confidenceLevel
+      | otherwise =
+          estimateFromInterval pt (resample ! lo, resample ! hi) confidenceLevel
+      where
+        -- Quantile estimates for given CL
+        lo    = max (cumn a1) 0
+          where a1 = bias + b1 / (1 - accel * b1)
+                b1 = bias + z1
+        hi    = min (cumn a2) (ni - 1)
+          where a2 = bias + b2 / (1 - accel * b2)
+                b2 = bias - z1
+        -- Number of resamples
+        ni    = U.length resample
+        n     = fromIntegral ni
+        -- Corrections
+        z1    = quantile standard (significanceLevel confidenceLevel / 2)
+        cumn  = round . (*n) . cumulative standard
+        bias  = quantile standard (probN / n)
+          where probN = fromIntegral . U.length . U.filter (<pt) $ resample
+        accel = sumCubes / (6 * (sumSquares ** 1.5))
+          where (sumSquares :< sumCubes) = U.foldl' f (0 :< 0) jack
+                f (s :< c) j = s + d2 :< c + d2 * d
+                    where d  = jackMean - j
+                          d2 = d * d
+                jackMean     = mean jack
+        jack  = jackknife est sample
+
+
+-- | Basic bootstrap. This method simply uses empirical quantiles for
+--   confidence interval.
+basicBootstrap
+  :: (G.Vector v a, Ord a, Num a)
+  => CL Double       -- ^ Confidence vector
+  -> Bootstrap v a   -- ^ Estimate from full sample and vector of
+                     --   estimates obtained from resamples
+  -> Estimate ConfInt a
+{-# INLINE basicBootstrap #-}
+basicBootstrap cl (Bootstrap e ests)
+  = estimateFromInterval e (sorted ! lo, sorted ! hi) cl
+  where
+    sorted = gsort ests
+    n  = fromIntegral $ G.length ests
+    c  = n * (significanceLevel cl / 2)
+    -- FIXME: can we have better estimates of quantiles in case when p
+    --        is not multiple of 1/N
+    --
+    -- FIXME: we could have undercoverage here
+    lo = round c
+    hi = truncate (n - c)
+
+-- $references
+--
+-- * Davison, A.C; Hinkley, D.V. (1997) Bootstrap methods and their
+--   application. <http://statwww.epfl.ch/davison/BMA/>
