88b1251de46456bae4124197ecf1f0aa7b4b34d2
Vincent Hanquez <vincent@snarc.org>
2017-10-30 10:24:34 +0000

    add a slow bench

--- benchs/Main.hs
+++ benchs/Main.hs
@@ -1,7 +1,19 @@
+{-# LANGUAGE BangPatterns #-}
 module Main where
 
 import Gauge.Main
+import System.IO.Unsafe
+import Control.Concurrent
+import Control.Exception
+
+delayed :: (a -> b) -> a -> b
+delayed f a = unsafePerformIO $ do
+    !b <- evaluate (f a)
+    threadDelay 10000
+    pure b
+{-# NOINLINE delayed #-}
 
 main = defaultMain
-    [ bench "identity" $ whnf (map (+ 1)) [1,2,3 :: Int]
+    [ bench "identity" $ nf (map (+ 1)) [1,2,3 :: Int]
+    , bench "slow" $ nf (map (\i -> delayed (+ 1))) [1..10::Int]
     ]
