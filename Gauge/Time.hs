f0a781020efcb990b914a00f26839d99043bd8dc
Vincent Hanquez <vincent@snarc.org>
2017-11-15 09:32:07 +0000

    Add some simple convertion functions

--- Gauge/Time.hs
+++ Gauge/Time.hs
@@ -3,10 +3,13 @@
 {-# LANGUAGE DeriveGeneric #-}
 module Gauge.Time
     ( MicroSeconds(..)
-    , NanoSeconds(..)
-    , microSecondsToDouble
     , MilliSeconds(..)
+    , NanoSeconds(..)
+    -- * Convertion functions
+    , microSecondsToDouble
     , milliSecondsToDouble
+    , nanoSecondsToDouble
+    , doubleToNanoSeconds
     ) where
 
 import           Data.Typeable
@@ -14,6 +17,10 @@ import           Data.Data
 import           Data.Word
 import           Control.DeepSeq
 import           GHC.Generics
+
+-- | Represent a number of milliseconds.
+newtype MilliSeconds = MilliSeconds Word64
+    deriving (Eq, Read, Show, Typeable, Data, Generic, NFData, Enum, Bounded, Num)
 
 -- | Represent a number of microseconds
 newtype MicroSeconds = MicroSeconds Word64
@@ -29,12 +36,17 @@ ref_nanoseconds = 1000000000
 ref_microseconds :: Num a => a
 ref_microseconds = 1000000
 
-microSecondsToDouble :: MicroSeconds -> Double
-microSecondsToDouble (MicroSeconds w) = fromIntegral w / 1000000
+ref_milliseconds :: Num a => a
+ref_milliseconds = 1000
 
--- | Represent a number of milliseconds.
-newtype MilliSeconds = MilliSeconds Word64
-    deriving (Eq, Read, Show, Typeable, Data, Generic, NFData, Enum, Bounded, Num)
+microSecondsToDouble :: MicroSeconds -> Double
+microSecondsToDouble (MicroSeconds w) = fromIntegral w / ref_microseconds
 
 milliSecondsToDouble :: MilliSeconds -> Double
-milliSecondsToDouble (MilliSeconds w) = fromIntegral w / 1000
+milliSecondsToDouble (MilliSeconds w) = fromIntegral w / ref_milliseconds
+
+nanoSecondsToDouble :: NanoSeconds -> Double
+nanoSecondsToDouble (NanoSeconds w) = fromIntegral w / ref_nanoseconds
+
+doubleToNanoSeconds :: Double -> NanoSeconds
+doubleToNanoSeconds w = NanoSeconds $ truncate (w * ref_nanoseconds)
